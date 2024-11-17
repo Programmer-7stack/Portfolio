c86e1a7d1166f8d96d26d03f8fa916b9b719683e
Programmer <bleartsejdiu4@gmail.com>
2024-11-17 14:42:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// JavaScript function to show an alert message
 function showMessage() {
-    alert('Ju keni kryer me sukses blerjen në faqe');
+    const modal = document.getElementById("cardModal");
+    modal.style.display = "flex"; // Show the modal
 }
 
+function closeModal() {
+    const modal = document.getElementById("cardModal");
+    modal.style.display = "none"; // Hide the modal
+}
 
-// Funksioni për kërkimin e produkteve
-function searchProducts() {
-    var searchInput = document.getElementById("searchInput").value.toLowerCase(); // Merr tekstin e kërkimit dhe e bën të vogël
-    var productCards = document.querySelectorAll(".product-card"); // Merr të gjitha produktet
-
-    productCards.forEach(function(card) {
-        var productName = card.getAttribute("data-name").toLowerCase(); // Merr emrin e produktit
-        if (productName.includes(searchInput)) {
-            card.style.display = "block"; // Nëse emri i produktit përmban tekstin e kërkuar, shfaq produktin
-        } else {
-            card.style.display = "none"; // Ndryshe, fshi produktin
-        }
-    });
+function processPayment(event) {
+    event.preventDefault();
+    alert("Blerja e produktit eshte kryer me sukses!");
+    closeModal(); // Close the modal after payment
 }
